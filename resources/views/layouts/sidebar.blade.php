b0ce701847eb0cd35a49aba23d0b087a5141560a
AYUMI CHINTYA <ayumichintya1886@gmail.com>
2026-03-15 16:46:31 +0700

    Update Dashboard Layouts. Sidebar dan Navbar

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class="sidebar">
+
+<div class="logo">
+<img src="{{ asset('images/logo.png') }}" alt="Logo Stok Gudang">
+</div>
+
+<ul class="menu">
+
+    <li>
+        <a href="#">Beranda</a>
+    </li>
+
+    <li>
+        <a href="#">Kelola Supplier</a>
+    </li>
+
+    <li>
+        <a href="#">Kelola Barang</a>
+    </li>
+
+    <li class="active">
+        <a href="#">Kelola Kategori</a>
+    </li>
+
+    <li>
+        <a href="#">Riwayat</a>
+    </li>
+
+</ul>
+
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/login', [LoginController::class, 'login']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
